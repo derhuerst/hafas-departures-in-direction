fe08c5a787c753772c9d1d6781880843a4c73c9d
Jannis R <mail@jannisr.de>
2017-08-17 00:05:36 +0200

    implementation :clap:

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
 'use strict'
 
-// todo
+const departures = require('vbb-hafas/lib/departures')
+const journeyPart = require('vbb-hafas/lib/journey-part')
+
+const minute = 60 * 1000
+
+const defaults = {
+	concurrency: 4,
+	results: 10,
+	maxQueries: 10
+}
+
+const depsInDirection = (station, direction, opt = {}) => {
+	return new Promise((yay, nay) => {
+		opt = Object.assign({}, defaults, opt)
+
+		const res = []
+		let when = Date.now()
+		let queries = 0
+		const queue = new Queue({concurrency: opt.concurrency})
+
+		const checkDep = (dep) => () => {
+			if (res.length >= opt.results && queries >= opt.maxQueries) {
+				return Promise.resolve() // abort, we already have enough
+			}
+
+			// todo: vbb-hafas doesn't expose ref yet
+			return journeyPart(dep.ref, dep.line.name, {when})
+			.then((journey) => {
+				// todo: use stationOf index?
+				const i = journey.passed.findIndex(p => p.station.id === station)
+				const next = journey.passed[i + 1]
+				if (next && next.station.id === direction) res.push(dep)
+			})
+		}
+
+		const loop = () => {
+			departures(station, {when: new Date(when), duration: 5 * minute})
+			.then((deps) => {
+				for (let dep of deps) queue.add(checkDep(dep))
+				return queue.onEmpty()
+			})
+			.then(() => {
+				// todo: opt.timeout?
+				if (res.length >= opt.results && queries >= opt.maxQueries) yay(res)
+				else loop()
+			})
+			.catch(nay)
+
+			when += 5 * minute
+			queries++
+		}
+	})
+}
+
+module.exports = depsInDirection
