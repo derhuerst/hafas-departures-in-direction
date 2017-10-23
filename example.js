185491cfc9d6aae21e2e9ce710b0e5f59b65ddd8
Jannis R <mail@jannisr.de>
2017-10-24 01:09:13 +0200

    vbb -> hafas, part 1

--- example.js
+++ example.js
@@ -1,6 +1,9 @@
 'use strict'
 
-const depsInDirection = require('.')
+const departures = require('vbb-hafas/lib/departures')
+const journeyPart = require('vbb-hafas/lib/journey-part')
+
+const setup = require('.')
 
 const friedrichstr = '900000100001'
 const brandenburgerTor = '900000100025'
@@ -8,6 +11,8 @@ const leopoldplatz = '900000009102'
 const nauenerPlatz = '900000009201'
 const ostbahnhof = '900000120005'
 const jannowitzbruecke = '900000100004'
+
+const depsInDirection = setup(departures, journeyPart)
 
 depsInDirection(friedrichstr, brandenburgerTor)
 // depsInDirection(leopoldplatz, nauenerPlatz)
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 'use strict'
 
 const Queue = require('p-queue')
-const departures = require('vbb-hafas/lib/departures')
-const journeyPart = require('vbb-hafas/lib/journey-part')
 const omit = require('lodash.omit')
 
 const minute = 60 * 1000
@@ -13,60 +11,64 @@ const defaults = {
 	maxQueries: 10
 }
 
-const depsInDirection = (station, direction, opt = {}) => {
-	return new Promise((yay, nay) => {
-		opt = Object.assign({}, defaults, opt)
+const setup = (departures, journeyPart) => {
+	const depsInDirection = (station, direction, opt = {}) => {
+		return new Promise((yay, nay) => {
+			opt = Object.assign({}, defaults, opt)
 
-		const res = []
-		const queue = new Queue({concurrency: opt.concurrency})
+			const res = []
+			const queue = new Queue({concurrency: opt.concurrency})
 
-		let when = Date.now()
-		if (opt.when) {
-			if (Number.isNaN(+opt.when)) throw new Error('invalid when parameter')
-			when = +opt.when
-		}
-		let queries = 0, stop = false
+			let when = Date.now()
+			if (opt.when) {
+				if (Number.isNaN(+opt.when)) throw new Error('invalid when parameter')
+				when = +opt.when
+			}
+			let queries = 0, stop = false
 
-		const checkDep = (dep) => () => {
-			if (stop) return Promise.resolve()
+			const checkDep = (dep) => () => {
+				if (stop) return Promise.resolve()
 
-			return journeyPart(dep.ref, dep.line.name, {when})
-			.then((journey) => {
-				// todo: use stationOf index?
-				const i = journey.passed.findIndex(p => p.station.id === station)
-				const next = journey.passed[i + 1]
-				if (next && next.station.id === direction) res.push(dep)
-			})
-			.catch((err) => {
-				// todo: retry
-				if (!err.statusCode) nay(err) // ignore HTTP errors
-			})
-		}
+				return journeyPart(dep.ref, dep.line.name, {when})
+				.then((journey) => {
+					// todo: use stationOf index?
+					const i = journey.passed.findIndex(p => p.station.id === station)
+					const next = journey.passed[i + 1]
+					if (next && next.station.id === direction) res.push(dep)
+				})
+				.catch((err) => {
+					// todo: retry
+					if (!err.statusCode) nay(err) // ignore HTTP errors
+				})
+			}
 
-		const loop = () => {
-			const _opt = omit(opt, ['concurrency', 'results', 'maxQueries'])
-			_opt.when = when
-			_opt.duration = 5
+			const loop = () => {
+				const _opt = omit(opt, ['concurrency', 'results', 'maxQueries'])
+				_opt.when = when
+				_opt.duration = 5
 
-			departures(station, _opt)
-			.then((deps) => {
-				for (let dep of deps) queue.add(checkDep(dep))
-				return queue.onEmpty()
-			})
-			.then(() => {
-				// todo: opt.timeout?
-				if (res.length >= opt.results || queries >= opt.maxQueries) {
-					stop = true
-					yay(res)
-				} else loop()
-			})
-			.catch(nay)
+				departures(station, _opt)
+				.then((deps) => {
+					for (let dep of deps) queue.add(checkDep(dep))
+					return queue.onEmpty()
+				})
+				.then(() => {
+					// todo: opt.timeout?
+					if (res.length >= opt.results || queries >= opt.maxQueries) {
+						stop = true
+						yay(res)
+					} else loop()
+				})
+				.catch(nay)
 
-			when += 5 * minute
-			queries++
-		}
-		loop()
-	})
+				when += 5 * minute
+				queries++
+			}
+			loop()
+		})
+	}
+
+	return depsInDirection
 }
 
-module.exports = depsInDirection
+module.exports = setup
